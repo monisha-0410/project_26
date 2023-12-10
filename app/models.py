4380d93349681bea239ed8a92c26160bf89690ec
monisha0410 <monishashalini2002@gmail.com>
2023-12-10 01:30:17 -0800

    first commit

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Dept(models.Model):
+    DEPTNO=models.IntegerField(primary_key=True)
+    DNAME=models.CharField(max_length=100)
+    Loc=models.CharField(max_length=100)
+    
+class Emp(models.Model):
+    EMPNO=models.IntegerField(primary_key=True)
+    Ename=models.CharField(max_length=100)
+    Job=models.CharField(max_length=100)
+    Mgr=models.IntegerField()
+    HIREDATE=models.DateField()
+    Sal=models.IntegerField()
+    comm=models.IntegerField()
+    DEPTNO=models.ForeignKey(Dept,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.Ename
